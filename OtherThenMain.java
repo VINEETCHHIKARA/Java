d9426d7110a8c9c6d161af1951f2ab87cc023ec6
VINEET CHAUDHARY <vineet.1614077@kiet.edu>
2018-08-19 23:19:37 +0530

    Without Write some code in main method

---
+++ OtherThenMain.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class OtherThenMain {
+	
+	static {
+		System.out.print("Demo For Static Block");
+		System.exit(1);
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
